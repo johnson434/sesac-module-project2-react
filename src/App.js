c10cd596e7ca8505e03116073ae99ff1f40d18c2
johnson434 <ghkddlsrb96@gmail.com>
2024-09-27 11:50:08 +0900

    백엔드 서버에 요청 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, { useState } from 'react';
+
+const URL = "http://test.com/api"
 
 function App() {
+  const [apiStatusCode, setApiStatusCode] = useState();
+
+  const getRoot = async () => {
+    axios.get(URL).then((response) => {
+      setApiStatusCode(response.status);
+    }).catch((reason) => {
+      console.log("catch문");
+      console.log(reason);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +29,10 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <button onClick={getRoot}>request 버튼</button>
+          <span>response status code: {apiStatusCode}</span>
+        </div>
       </header>
     </div>
   );
